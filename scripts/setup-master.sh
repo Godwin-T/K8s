ca7d081a918ac3f1f73f0f7b167f160e8d7524f9
Godwin-T <godwinolagunju0@gmail.com>
2025-04-30 11:54:52 +0100

    initial commit

---
+++ scripts/setup-master.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt install -y git nano 
+
+sudo ./setup-container.sh
+sudo ./setup-kubetools.sh
+
+sudo kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
---
+++ scripts/setup-node.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install -y git nano 
+
+sudo ./setup-container.sh
+sudo ./setup-kubetools.sh
+
