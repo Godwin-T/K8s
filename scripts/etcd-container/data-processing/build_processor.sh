f7b63908a83d706f1bcc521a0a5d14bede4d64c4
Godwin-T <godwinolagunju0@gmail.com>
2025-05-11 22:21:30 +0100

    update repo

--- scripts/etcd-container/data-processing/build_processor.sh
+++
@@ -1,7 +0,0 @@
-# Build the Docker image
-docker build -t data-processor:latest -f Dockerfile-data-processor .
-
-# Tag and push the image to your registry
-docker tag data-processor:latest freshinit/data-processor:latest
-docker push freshinit/data-processor:latest
-
--- scripts/setup-node.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-sudo apt install -y git nano 
-
-sudo ./setup-container.sh
-sudo ./setup-kubetools.sh
-
