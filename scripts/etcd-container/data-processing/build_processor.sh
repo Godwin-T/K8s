478a167237941ad6de15efc446f764264c65bd32
Godwin-T <godwinolagunju0@gmail.com>
2025-05-11 06:13:05 +0100

    update script

--- scripts/etcd-container/data-processing/build_processor.sh
+++ scripts/etcd-container/data-processing/build_processor.sh
@@ -2,8 +2,8 @@
 docker build -t data-processor:latest -f Dockerfile-data-processor .
 
 # Tag and push the image to your registry
-docker tag data-processor:latest ${DOCKER_REGISTRY}/data-processor:latest
-docker push ${DOCKER_REGISTRY}/data-processor:latest
+docker tag data-processor:latest freshinit/data-processor:latest
+docker push freshinit/data-processor:latest
 
 # Deploy the processor
 kubectl apply -f data-processor-deployment.yaml
--- scripts/etcd-container/metadata-collector/build_collector.sh
+++ scripts/etcd-container/metadata-collector/build_collector.sh
@@ -2,8 +2,8 @@
 docker build -t metadata-collector:latest -f Dockerfile-metadata-collector .
 
 # Tag and push the image to your registry
-docker tag metadata-collector:latest ${DOCKER_REGISTRY}/metadata-collector:latest
-docker push ${DOCKER_REGISTRY}/metadata-collector:latest
+docker tag metadata-collector:latest freshinit/metadata-collector:latest
+docker push freshinit/metadata-collector:latest
 
 # Deploy the collector
 kubectl apply -f metadata-collector-deployment.yaml
